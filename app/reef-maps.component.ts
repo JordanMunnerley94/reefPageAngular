3e6cd749a755e34b3dd0cb9cbc9ff86100d2f9ce
philipbowrey <philipbowrey@gmail.com>
2016-12-05 09:26:04 +1000

    Can get maps - no if statement yet - now working on integrating wms

--- app/reef-maps.component.ts
+++ app/reef-maps.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from "@angular/core";
 import {ReefPageService} from "./reef-page.service";
 import {ActivatedRoute} from "@angular/router";
 
-declare var googleApi : any;
+declare var google : any;
 
 @Component({
     moduleId: module.id,
@@ -16,7 +16,7 @@ declare var googleApi : any;
 export class ReefMapsComponent implements OnInit {
 
     id: string;
-    reefMapBounds: any[];
+    reefMapBounds: any;
 
     constructor(
         private reefPageService : ReefPageService,
@@ -26,14 +26,46 @@ export class ReefMapsComponent implements OnInit {
         this.route.params.subscribe(params => {
             this.id = params['reefid'];
             this.getReefData(this.id);
+            // this.drawMaps(this.reefMapBounds)
         });
+
+    }
+
+    drawMaps(data: any) {
+        console.log("In draw maps", data);
+
+        let mapCenter = new google.maps.LatLng(-14.64452, 145.45511);
+
+        let myOptions = {
+            zoom: 6,
+            center: mapCenter,
+            mapTypeId: google.maps.MapTypeId.HYBRID,
+            disableDefaultUI: true,
+            disableDoubleClickZoom: true,
+            draggable: false,
+            scrollwheel: false,
+            panControl: false
+        }
+
+        // Creates maps
+        let overviewMap = new google.maps.Map(document.getElementById('overview-map'), myOptions);
+        let detailedMap = new google.maps.Map(document.getElementById('detailed-map'), myOptions);
+
+        // Sets lat-lng for detailed map
+        let detailedBounds = new google.maps.LatLngBounds();
+        detailedBounds.extend(new google.maps.LatLng(data.north, data.east));
+        detailedBounds.extend(new google.maps.LatLng(data.south, data.west));
+        detailedMap.fitBounds(detailedBounds);
+
+
+
+
     }
 
     getReefData(id: string): void {
         this.reefPageService.getData(id).then(reefData => {
-            console.log(reefData)
             this.reefMapBounds = reefData.mantaPathBound;
-            console.log(this.reefMapBounds)
+            this.drawMaps(this.reefMapBounds)
         })
     };
 
